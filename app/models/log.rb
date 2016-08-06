5137b8467e7ee9276be1e209a5d5a7d302333c85
Jade Vance <jadenicolevance@gmail.com>
2016-08-06 15:39:11 -0700

    added in routing endpoints and methods to the users controller. Added to the logging and user models

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,15 @@
 class Log < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+
+  # validates :request, presence: true
+  # validates :response, presence: true
+
+  # def self.create_by(request, response)
+  #   log = Log.new
+  #   log.request  = request.body.read
+  #   log.response = response.body
+  #   if log.save
+  #     return log 
+  #   end 
+  # end 
 end
