a3fc8bba1e4b754ab9deab726192586f5c6bd026
Jade Vance <jadenicolevance@gmail.com>
2016-08-09 09:16:53 -0700

    working on the API recieving a request and creating a user record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,25 @@
 class UsersController < ApplicationController
-  config.force_ssl = true
+  
+  force_ssl
 
   def create
     if request.post?
-      @user = User.find(:api_token => params[:api_authtoken]).first
-    
+      api_token = params[:api_authtoken]
+      User.find_or_create_new_user(api_token)
+      # if nil, return an error "unable to save, please try again later"
     render :json [@user]
     end
   end
 
+# If there is a token in the database, return their profile info 
+#  and control goes back to iOS (main page)
+
+# If there is no token, User.new and create a new DB entry + upload a new photo to S3
+# control goes back to iOS (main page)
+
+
   private 
+  # params permit
   # searches based on location here ? 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
   has_many :dogs
   has_many :logs   
+  def create_new_user(api_token)
+    user = self.find_by(uid: auth_hash['info']['uid'], provider: auth_hash['provider'])
+    if !user.nil?
+      return user
+    else
+        user                  = User.new 
+        user.provider         = auth_hash.provider
+        user.uid              = auth_hash.uid
+        user.name             = auth_hash.info.name
+        user.image            = auth_hash.info.image 
+        user.oauth_token      = auth_hash.credentials.token
+        user.oauth_expires_at = Time.at(auth_hash.credentials.expires_at)
+        user.save!
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end 
 end
