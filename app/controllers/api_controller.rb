2b1f3e3074f7be3072d6b1579f57609ae1ea1704
Jade Vance <jadenicolevance@gmail.com>
2016-08-10 14:57:15 -0700

    forgot to remove the raise, naturally things broke

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,6 @@ class ApiController < ApplicationController
   def index
     if request.post?
       @userID = params 
-      raise
       render json: [@userID]
     else
       render json: ["This is a response from the API. Hello!"]
