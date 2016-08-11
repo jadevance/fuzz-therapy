02a6acf199e6a108f8280c94c7e2696a74b71653
Jade Vance <jadenicolevance@gmail.com>
2016-08-11 08:42:59 -0700

    testing assumptions about JSON

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,7 @@ class ApiController < ApplicationController
   def index
     if request.post?
       userID = params[:user]
-      user = User.find_user(userID)
+      # user = User.find_user(userID)
       render json: [user]
     else
       render json: ["This is a response from the API. Hello!"]
