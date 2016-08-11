41325395a5f8488767f297b0b945655fcb702dc4
Jade Vance <jadenicolevance@gmail.com>
2016-08-11 08:41:42 -0700

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
 class User < ApplicationRecord
 
   def self.find_user(uid) 
-    user = self.find_by(uid: auth_hash['info']['uid'])
-    # if !user.nil?
+    user = self.find_by(uid: auth_hash['user']['uid'])
     return user
   end 
 
   def self.create_new_user(auth_hash)
       user                  = User.new 
       user.uid              = auth_hash.uid
-      user.name             = auth_hash.info.name
-      user.location         = auth_hash.info.image 
-      user.availability     = auth_hash.info.image 
-      user.dog_breed        = auth_hash.info.dog_breed
-      user.dog_name         = auth_hash.info.dog_name
-      user.dog_age          = auth_hash.info.dog_age
-      user.dog_picture      = auth_hash.info.dog_picture
+      user.name             = auth_hash.name
+      user.location         = auth_hash.image 
+      user.availability     = auth_hash.image 
+      user.dog_breed        = auth_hash.dog_breed
+      user.dog_name         = auth_hash.dog_name
+      user.dog_age          = auth_hash.dog_age
+      user.dog_picture      = auth_hash.dog_picture
       user.save!
       
     if user.save
@@ -25,3 +24,14 @@ class User < ApplicationRecord
     end
   end 
 end
+
+    # t.datetime "created_at",   null: false
+    # t.datetime "updated_at",   null: false
+    # t.string   "name",         null: false
+    # t.string   "location",     null: false
+    # t.string   "availability", null: false
+    # t.string   "uid",          null: false
+    # t.string   "dog_name",     null: false
+    # t.integer  "dog_age"
+    # t.string   "dog_breed"
+    # t.string   "dog_picture",  null: false
