9cefb8527b0c34030228d06e168c264bb4d9c73f
Jade Vance <jadenicolevance@gmail.com>
2016-08-11 11:28:05 -0700

    removed references to old tables

--- db/migrate/20160811153200_drop_tables_from_schema.rb
+++
@@ -1,8 +0,0 @@
-class DropTablesFromSchema < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :dogs
-    drop_table :logs
-    change_column_null :users, :dog_name, false 
-    change_column_null :users, :dog_picture, false 
-  end
-end
