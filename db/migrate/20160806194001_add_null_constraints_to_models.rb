71040c6257e0be74f7be560b7177286dc13a71af
Jade Vance <jadenicolevance@gmail.com>
2016-08-11 11:29:31 -0700

    removed more references to logs

--- db/migrate/20160806194001_add_null_constraints_to_models.rb
+++ db/migrate/20160806194001_add_null_constraints_to_models.rb
@@ -7,9 +7,5 @@ class AddNullConstraintsToModels < ActiveRecord::Migration[5.0]
     change_column_null :users, :uid, false
     change_column_null :users, :oauth_token, false
     change_column_null :users, :oauth_expires_at, false
-    change_column_null :logs, :request, false 
-    change_column_null :logs, :response, false 
-    change_column_null :dogs, :dog_name, false 
-    change_column_null :dogs, :dog_picture, false 
   end
 end
