c7232b4a386bd9e1191c7970472eb87c5ebe7da2
Jade Vance <jadenicolevance@gmail.com>
2016-08-15 10:20:05 -0700

    updated model info

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ApplicationRecord
   end 
 
   def self.create_new_account(auth_hash)
-      user                  = User.new 
-      user.uid              = auth_hash["uid"]
-      user.name             = auth_hash["name"]
-      user.location         = auth_hash["location"]
-      user.availability     = auth_hash["availability"]
-      user.dog_breed        = auth_hash["dog_breed"]
-      user.dog_name         = auth_hash["dog_name"]
-      user.dog_age          = auth_hash["dog_age"]
-      user.dog_picture      = auth_hash["dog_picture"]
+      user               = User.new 
+      user.uid           = auth_hash["uid"]
+      user.name          = auth_hash["name"]
+      user.location      = auth_hash["location"]
+      user.availability  = auth_hash["availability"]
+      user.dog_breed     = auth_hash["dog_breed"]
+      user.dog_name      = auth_hash["dog_name"]
+      user.dog_age       = auth_hash["dog_age"]
+      user.dog_picture   = auth_hash["dog_picture"]
       user.save!
       
     if user.save
