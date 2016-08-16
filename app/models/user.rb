11cf34b6745b7fb19c7d1eef9dd2825903ca3e31
Jade Vance <jadenicolevance@gmail.com>
2016-08-16 13:32:09 -0700

    added validations back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
 
-  # This method associates the attribute ":avatar" with a file attachment
-  # has_attached_file :avatar, styles: {
-  #   thumb: '100x100>',
-  #   square: '200x200#',
-  #   medium: '300x300>'
-  # }
+  This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 
-  # # Validate the attached image is image/jpg, image/png, etc
-  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def self.find_user(uid) 
     user = User.find_by(uid: uid)
@@ -31,6 +31,7 @@ class User < ApplicationRecord
       
       # before action: picture url from S3
       # insert it into the PG database 
+
       user.dog_picture   = auth_hash["dog_picture"]
       user.save!
       
@@ -40,8 +41,8 @@ class User < ApplicationRecord
       return nil
     end
 
-    def upload_s3_photo(dog_picture)
-      user.avatar = dog_picture
+    def upload_s3_photo(dog_picture_data)
+      user.avatar = dog_picture_data
     end 
   end 
 end
