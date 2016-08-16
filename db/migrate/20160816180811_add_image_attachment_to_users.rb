a0bf9fae8bc78489c7df844fa7ec5dde47e3f998
Jade Vance <jadenicolevance@gmail.com>
2016-08-16 13:08:26 -0700

    fixing migrations

--- db/migrate/20160816180811_add_image_attachment_to_users.rb
+++ db/migrate/20160816180811_add_image_attachment_to_users.rb
@@ -1,8 +1,4 @@
 class AddImageAttachmentToUsers < ActiveRecord::Migration[5.0]
-  def change
-      remove_column :users, :dog_image
-  end
-
   def self.up
     add_attachment :users, :avatar
   end
--- db/migrate/20160816181805_adding_paperclip_again.rb
+++
@@ -1,9 +0,0 @@
-class AddingPaperclipAgain < ActiveRecord::Migration[5.0]
-  def self.up
-    add_attachment :users, :avatar
-  end
-
-  def self.down
-    remove_attachment :users, :avatar
-  end
-end
