4d1327473f77ac3e3e679f961f6cee09e1efaa87
Jade Vance <jadenicolevance@gmail.com>
2016-08-18 09:20:23 -0700

    serving back the correct (maybe) url

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,10 +41,10 @@ class ApiController < ApplicationController
     user = User.find_user(params[:uid])
     user.dog_picture = params[:dog_picture]
     wrong_url = user.dog_picture.url(:original)
-    wrong_url.gsub(/^http:\/\/s3.amazonaws.com\/jvance-fuzztherapy-assets/, 'jvance-fuzztherapy-assets.s3.amazonaws.com')
-    user.dog_picture_url = user.dog_picture.url(:original)
-    user.save! 
-    render json: [user.dog_picture.url(:original)] 
+    correct_url = wrong_url.gsub(/^http:\/\/s3.amazonaws.com\/jvance-fuzztherapy-assets/, 'jvance-fuzztherapy-assets.s3.amazonaws.com')
+    # user.dog_picture_url = user.dog_picture.url(:original)
+    # user.save! 
+    render json: [correct_url] 
   end 
 
   def search
