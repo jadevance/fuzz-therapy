2a7099e4190ec2e744cd9553275de1e93d05b320
Jade Vance <jadenicolevance@gmail.com>
2016-08-24 08:45:54 -0700

    created a migration to update the schema for email data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817221325) do
+ActiveRecord::Schema.define(version: 20160824153629) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",               null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160817221325) do
     t.integer  "dog_picture_file_size"
     t.datetime "dog_picture_updated_at"
     t.string   "dog_picture_url"
+    t.string   "email"
   end
 
 end
